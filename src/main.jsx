5c8420d204bf4105f6575c618b0115f8be6faf41
qkrdpdls0603-crypto <qkrdpdls0603@gmail.com>
2026-06-12 13:03:39 +0900

    260612

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
